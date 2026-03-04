4bee8361605f4e4ed227d3366ed5358b4af2d471
sharath010904 <sharathchandra010904@gmail.com>
2026-03-04 21:58:23 +0530

    Create dfs.java

---
+++ Graphs/dfs.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public void dfs(int node, boolean[] visited, ArrayList<ArrayList<Integer>> adj) {
+
+        visited[node] = true;
+        System.out.print(node + " ");
+
+        for (int neighbor : adj.get(node)) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, visited, adj);
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of vertices: ");
+        int V = sc.nextInt();
+
+        System.out.print("Enter number of edges: ");
+        int E = sc.nextInt();
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        System.out.println("Enter edges (u v):");
+        for (int i = 0; i < E; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            adj.get(u).add(v);
+            adj.get(v).add(u); 
+        }
+
+        boolean[] visited = new boolean[V];
+
+        Solution obj = new Solution();
+
+        System.out.println("DFS Traversal starting from node 0:");
+        obj.dfs(0, visited, adj);
+
+        sc.close();
+    }
+}
