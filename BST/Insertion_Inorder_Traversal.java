b61e6bba33b6e69fa31d3bc1e6a5131caa51bc11
sharath010904 <sharathchandra010904@gmail.com>
2026-04-14 19:32:30 +0530

    Create Insertion_Inorder_Traversal.java

---
+++ BST/Insertion_Inorder_Traversal.java
@@ -0,0 +1,54 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+class BST {
+    Node root;
+
+    // Insert node
+    Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        }
+
+        if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else if (data > root.data) {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    // Inorder Traversal (Left, Root, Right)
+    void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        BST tree = new BST();
+
+        tree.root = tree.insert(tree.root, 50);
+        tree.insert(tree.root, 30);
+        tree.insert(tree.root, 70);
+        tree.insert(tree.root, 20);
+        tree.insert(tree.root, 40);
+        tree.insert(tree.root, 60);
+        tree.insert(tree.root, 80);
+
+        System.out.println("Inorder Traversal:");
+        tree.inorder(tree.root);  // Output will be sorted
+    }
+}
